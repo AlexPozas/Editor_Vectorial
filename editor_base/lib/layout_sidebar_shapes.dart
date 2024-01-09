01008e0b83174c4ae3874828e5bc9a460abf1cbb
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2024-01-09 15:54:38 +0100

    9

--- editor_base/lib/layout_sidebar_shapes.dart
+++ editor_base/lib/layout_sidebar_shapes.dart
@@ -1,10 +1,28 @@
-import 'package:flutter/cupertino.dart';
+import 'package:editor_base/SidebarShapePainter.dart';
+import 'package:editor_base/util_shape.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app_data.dart';
 
+class MiniatureShape extends StatelessWidget {
+  final Shape shape;
+
+  MiniatureShape(this.shape);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 50, // Ajusta el ancho según sea necesario
+      height: 50, // Ajusta la altura según sea necesario
+      child: CustomPaint(
+        painter: SidebarShapePainter(shape),
+      ),
+    );
+  }
+}
+
 class LayoutSidebarShapes extends StatefulWidget {
-  const LayoutSidebarShapes({super.key});
+  const LayoutSidebarShapes({Key? key}) : super(key: key);
 
   @override
   LayoutSidebarShapesState createState() => LayoutSidebarShapesState();
@@ -15,7 +33,7 @@ class LayoutSidebarShapesState extends State<LayoutSidebarShapes> {
   Widget build(BuildContext context) {
     AppData appData = Provider.of<AppData>(context);
     return SizedBox(
-      width: double.infinity, // Estira el widget horitzontalment
+      width: double.infinity,
       child: Container(
         padding: const EdgeInsets.all(4.0),
         child: Column(
@@ -24,21 +42,35 @@ class LayoutSidebarShapesState extends State<LayoutSidebarShapes> {
             SizedBox(
               width: double.infinity,
               child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: appData.shapesList.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      child: Container(
-                          padding: EdgeInsets.all(8),
-                          color: Colors.transparent,
-                          child: Text(
-                              'Shape $index  ${appData.shapesList[index].strokeWidth}px')),
-                      onTap: () {
+                shrinkWrap: true,
+                itemCount: appData.shapesList.length,
+                itemBuilder: (context, index) {
+                  bool isSelected = appData.shapeSelectedPrevious == index;
+
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
                         appData.getRecuadre(appData.shapesList[index]);
                         appData.recuadre = true;
-                      },
-                    );
-                  }),
+                        appData.shapeSelectedPrevious = index;
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      color: isSelected ? Colors.blue : Colors.transparent,
+                      child: Row(
+                        children: [
+                          // Ajusta el espacio entre la miniatura y el texto
+                          Text(
+                              'Shape $index  ${appData.shapesList[index].strokeWidth}px'),
+                          SizedBox(width: 8),
+                          MiniatureShape(appData.shapesList[index]),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
